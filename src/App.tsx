f6e30aa6e97306a13564dd14b23ef0b910de8610
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-05-06 19:12:08 -0300

    feat: add ThemeProvider and ModeToggle for dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Header from "./components/Header"
+import { ModeToggle } from "./components/Header/DarkMode/mode-toggle";
+import Header from "./components/Header";
+import { ThemeProvider } from "@/components/Header/DarkMode/theme-provider";
 
 function App() {
   return (
-    <Header/>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Header />
+      <ModeToggle />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
